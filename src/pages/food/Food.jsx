f3962c28cd897c654aa3b296f833e2ee912f1060
ed-cavalcanti <edney2809@gmail.com>
2022-06-24 00:17:38 -0300

    [fix] - Refatora a lógica do requisito 33 para fazer requisição a API

--- src/pages/food/Food.jsx
+++ src/pages/food/Food.jsx
@@ -1,7 +1,5 @@
-import React, { useContext } from 'react';
-// import { useLocation } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { MeuContextoInterno } from '../../context/index';
 import YoutubeIcon from '../../images/youtube.svg';
 import shareIcon from '../../images/shareIcon.svg';
 import whiteHeartIcon from '../../images/whiteHeartIcon.svg';
@@ -10,13 +8,27 @@ import '../../component/recipeCard.css';
 const arrayIndex = ['recipe1', 'recipe2', 'recipe3', 'recipe4', 'recipe5', 'recipe6'];
 
 const Food = () => {
-  // const { state } = useLocation();
   const { id: urlId } = useParams();
+
+  const [recipeDetails, setRecipeDetails] = useState({});
   const {
-    recipes: { foods },
-  } = useContext(MeuContextoInterno);
-  const state = foods.find(({ idMeal }) => idMeal === urlId);
-  const { strMealThumb, strMeal, strCategory, strInstructions, strYoutube } = state;
+    strMealThumb,
+    strMeal,
+    strCategory,
+    strInstructions,
+    strYoutube,
+  } = recipeDetails;
+
+  useEffect(() => {
+    const getRecipe = async () => {
+      const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${urlId}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      setRecipeDetails(data.meals[0]);
+    };
+    getRecipe();
+  }, [urlId]);
+
   const filterIgredients = (recipe) => {
     const TWENTY = 20;
     const ingredientList = [];
@@ -33,71 +45,66 @@ const Food = () => {
 
     return ingredientList;
   };
+
   return (
     <div>
-      { state && (
-        <div>
-          <h1 data-testid="recipe-title" className="l-food"> Food </h1>
-          <img
-            className="card-img card-img-mine"
-            data-testid="recipe-photo"
-            src={ strMealThumb }
-            alt={ strMeal }
-          />
-          <button
-            type="button"
-            data-testid="share-btn"
+      <h1 data-testid="recipe-title" className="l-food">{ strMeal }</h1>
+
+      <img
+        className="card-img card-img-mine"
+        data-testid="recipe-photo"
+        src={ strMealThumb }
+        alt={ strMeal }
+      />
+
+      <button
+        type="button"
+        data-testid="share-btn"
+      >
+        <img src={ shareIcon } alt="Share" className="share-icon" />
+      </button>
+
+      <button
+        type="button"
+        data-testid="favorite-btn"
+      >
+        <img src={ whiteHeartIcon } alt="Favorite" className="favorite-icon" />
+      </button>
+
+      <p data-testid="recipe-category">{strCategory}</p>
+
+      <ul>
+        {filterIgredients(recipeDetails).map((ingredient, index) => (
+          <li
+            key={ index }
+            data-testid={ `${index}-ingredient-name-and-measure` }
           >
-            <img src={ shareIcon } alt="Share" className="share-icon" />
-          </button>
-          <button
-            type="button"
-            data-testid="favorite-btn"
-          >
-            <img src={ whiteHeartIcon } alt="Favorite" className="favorite-icon" />
-          </button>
-          <p data-testid="recipe-category">{strCategory}</p>
-          <ol>
-            {filterIgredients(state).map((ingredient, index) => (
-              <li
-                key={ index }
-                data-testid={ `${index}-ingredient-name-and-measure` }
-              >
-                { ingredient }
-              </li>
-            ))}
+            { ingredient }
+          </li>
+        ))}
+      </ul>
 
-          </ol>
+      <p data-testid="instructions">{strInstructions}</p>
 
-          <p data-testid="instructions">{strInstructions}</p>
-          <a
-            data-testid="video"
-            href={ strYoutube }
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={ YoutubeIcon } alt="Youtube" className="youtube-icon" />
-          </a>
-          {/* <div data-testid="1-recomendation-card">
-        <recipeCard />
-      </div>
-      <div data-testid="2-recomendation-card">
-        <recipeCard />
+      <a
+        data-testid="video"
+        href={ strYoutube }
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={ YoutubeIcon } alt="Youtube" className="youtube-icon" />
+      </a>
 
-      </div> */}
-          {arrayIndex.map((item, index) => (
-            <h3 key={ index } data-testid={ `${index}-recomendation-title` }>{item}</h3>
-          ))}
+      {arrayIndex.map((item, index) => (
+        <h3 key={ index } data-testid={ `${index}-recomendation-card` }>{item}</h3>
+      ))}
 
-          <button
-            type="button"
-            data-testid="start-recipe-btn"
-          >
-            Start
-          </button>
-
-        </div>
-      )}
+      <button
+        type="button"
+        data-testid="start-recipe-btn"
+      >
+        Start
+      </button>
     </div>
   );
 };
