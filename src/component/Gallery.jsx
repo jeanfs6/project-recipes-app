ff6b9e10aff8b1507fa29742619ffde7aed3c716
João Pster <joaopsterdev@gmail.com>
2022-06-25 10:09:26 -0300

    Instalado o React Responsive Carousel

--- src/component/Gallery.jsx
+++ src/component/Gallery.jsx
@@ -1,43 +1,25 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-responsive-carousel';
 import RecomendationCard from './RecomendationCard';
 
 // Galeria
 export default function Gallery({ recipes, type }) {
   return (
-    <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
-      <div className="carousel-inner">
-        {
-          recipes.map((recipe, index) => (
-            <div key={ index } className="carousel-item active">
-              <RecomendationCard
-                recipe={ recipe }
-                type={ type }
-                index={ index }
-              />
-            </div>
-          ))
-        }
-      </div>
-      <a
-        className="carousel-control-prev"
-        href="#carouselExampleControls"
-        role="button"
-        data-slide="prev"
-      >
-        <span className="carousel-control-prev-icon" aria-hidden="true" />
-        <span className="sr-only">Previous</span>
-      </a>
-      <a
-        className="carousel-control-next"
-        href="#carouselExampleControls"
-        role="button"
-        data-slide="next"
-      >
-        <span className="carousel-control-next-icon" aria-hidden="true" />
-        <span className="sr-only">Next</span>
-      </a>
-    </div>
+    <Carousel>
+      {
+        recipes.map((recipe, index) => (
+          <div key={ index }>
+            <RecomendationCard
+              recipe={ recipe }
+              type={ type }
+              index={ index }
+            />
+          </div>
+        ))
+      }
+    </Carousel>
   );
 }
 
