9f3d4f8c0a4361cea6568ef39997eb4dedfaad89
ed-cavalcanti <edney2809@gmail.com>
2022-06-29 22:00:45 -0300

    [fix] - limpa mock doneRecipes do login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -32,30 +32,7 @@ const Login = ({ history }) => {
     localApi.setLocalKey('mealsToken', '1');
     localApi.setLocalKey('cocktailsToken', '1');
     localApi.setLocalKey('user', { email });
-    localApi.setLocalKey('doneRecipes', [
-      {
-        id: '52771',
-        type: 'food',
-        nationality: 'Italian',
-        category: 'Vegetarian',
-        alcoholicOrNot: '',
-        name: 'Spicy Arrabiata Penne',
-        image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
-        doneDate: '23/06/2020',
-        tags: ['Pasta', 'Curry'],
-      },
-      {
-        id: '178319',
-        type: 'drink',
-        nationality: '',
-        category: 'Cocktail',
-        alcoholicOrNot: 'Alcoholic',
-        name: 'Aquamarine',
-        image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
-        doneDate: '23/06/2020',
-        tags: [],
-      },
-    ]);
+    localApi.setLocalKey('doneRecipes', []);
     localApi.setLocalKey('favoriteRecipes', []);
     localApi.setLocalKey('inProgressRecipes', { cocktails: {}, meals: {} });
     history.push('/foods');
